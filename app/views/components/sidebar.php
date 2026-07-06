4e30167ce882d8551398b0eae0070cd495c7b45d
manh <blumanh04@gmail.com>
2026-07-07 00:28:10 +0700

    .2

--- app/views/components/sidebar.php
+++ app/views/components/sidebar.php
@@ -6,7 +6,8 @@
          <li><a href="<?=BASE_URL?>/Reports"> 📄Báo cáo thống kê</a></li>
         <li><a href="<?=BASE_URL?>/Games">🎮 Quản lý trò chơi</a></li>
         <li><a href="<?=BASE_URL?>/User"> 👤Quản lý người dùng</a></li>
-        <li><a href="<?=BASE_URL?>/Ticket">🎫 Quản lý vé</a></li>
+        <li><a href="<?=BASE_URL?>/Ticket">🎫 Quản lý vé đã bán</a></li>
+        <li><a href="<?=BASE_URL?>/Gate_tickets">🎟️ Quản lý loại vé cổng</a></li>
         <li><a href="<?=BASE_URL?>/Feedback">💬 Đánh giá</a></li>
         <li><a href="<?=BASE_URL?>/Promotions">🎁 Quản lý khuyến mãi</a></li>
         <li><a href="<?=BASE_URL?>/login" class="logout">🚪 Đăng xuất</a></li>
