952e2e16e5d8e49973ccc62933c81586663ac30f
manh <blumanh04@gmail.com>
2026-07-15 13:41:03 +0700

    .

--- backend/api-gateway/src/main.ts
+++ backend/api-gateway/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   createProxyMiddleware({
     target: userServiceUrl,
     changeOrigin: true,
-    pathRewrite: { '^/api/auth': '/auth' },
+    pathRewrite: { '^/': '/auth' },
   }),
 );
 
@@ -30,7 +30,7 @@ app.use(
   createProxyMiddleware({
     target: userServiceUrl,
     changeOrigin: true,
-    pathRewrite: { '^/api/users': '/users' },
+    pathRewrite: { '^/': '/users' },
   }),
 );
 
