2639c12321d8a4c7170634735157b8aef2d4c859
manh <blumanh04@gmail.com>
2026-07-28 15:26:58 +0700

    CICD 6

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -1,29 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
-    UploadModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UploadModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
