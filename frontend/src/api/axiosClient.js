ef8a5a1082d01ef5cd6e58cf37fef5400e7184c6
manh <blumanh04@gmail.com>
2026-07-29 09:52:23 +0700

    axios fix

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: "/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken');
+  const token = localStorage.getItem("accessToken");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
