5169c6a4ef0e1c86ab04aa52e201a611577f9443
manh <blumanh04@gmail.com>
2026-09-09 23:58:29 +0700

    fix binh luan

--- backend/services/game-service/src/games/dto/create-feedback.dto.ts
+++ backend/services/game-service/src/games/dto/create-feedback.dto.ts
@@ -10,11 +10,11 @@ import {
 export class CreateFeedbackDto {
   @IsInt()
   @Min(1)
+  @Max(5)
   rating!: number;
 
   @IsString()
   @IsNotEmpty({ message: 'Nội dung đánh giá không được để trống' })
-  @Max(5)
   @MaxLength(1000, { message: 'Nội dung đánh giá tối đa 1000 ký tự' })
   content!: string;
 }
